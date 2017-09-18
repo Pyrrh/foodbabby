3f082cdfdd68d65676be15cbfd4c385e5cd73b2c
Pyrrh <Pyrrh@users.noreply.github.com>
2017-09-17 20:39:56 -0700

    Skeleton of Recipe class (#1)
    Structure for recipe class in place.

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#imports
+from Recipe import Recipe
+
+
+if __name__ == "__main__":
+    a_recipe = Recipe()
+
+
+    print("") #placeholder for later kthxbai
---
+++ tst/main_test.py
@@ -0,0 +1,1 @@
+#Import testing suite & do the things
