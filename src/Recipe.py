29304a767b6d6f67d4da36e5cc8c80bcfa10d867
Pyrrh <kurzen2@pdx.edu>
2017-09-17 20:35:03 -0700

    Structure for recipe class in place. Ready for pull request

--- src/Recipe.py
+++ src/Recipe.py
@@ -4,14 +4,15 @@ class Recipe(object):
         #self.quantity = 0
         #self.unit = ""
         #self.ingredient = ""
-        self.ingredients = [(0, "", "")] #need to store lots of qty & unit & ingred per recipe
+        self.ingredients = [] #need to store lots of qty & unit & ingred per recipe. will be ordered triple
         self.directions = "" #how to make this food
         self.notes = "" #personal annotations re: this recipe
         self.recipe_name = "" #what's in a name?
         self.synopsis = "" #prep time, cook time, bake temp, etc. also short note for easy ref if you want
         self.uses = 0 #track how many times a recipe has been used
         self.source = "" #where did the recipe come from?
-        
+        self.labels = [] #used in searching for recipes
+
 #these methods should interact with the database
     #do we need these to go somewhere else? do they make sense as part of this class?
     def add_recipe(self):
