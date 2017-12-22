f927a57fe2f0ec5db334118553aa7a8d8f35d0d6
Pyrrh <kurzen2@pdx.edu>
2017-12-21 20:17:27 -0800

    workin on a menuuuu

--- src/main.py
+++ src/main.py
@@ -1,9 +1,27 @@
 #imports
-from Recipe import Recipe
+from .Recipe import Recipe
+import Utilities
 
 
 if __name__ == "__main__":
     a_recipe = Recipe()
 
+    isValid = False
 
-    print("") #placeholder for later kthxbai
+
+    print("MAIN MENU\n\n")
+    print("Select an option: \n")
+
+    print("""
+    1. Add a recipe (NOT IMPLEMENTED)
+    2. Find a recipe (NOT IMPLEMENTED)
+    3. Build a shopping list (NOT IMPLEMENTED)
+    4. Save and Quit (SAVE NOT IMPLEMENTED, QUITTING IS EASY)
+    
+    """)
+    #refactor to have menu display during a single function call
+
+    while isValid != True:
+        isValid = Utilities.validate(input())
+
+
