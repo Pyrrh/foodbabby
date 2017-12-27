e88d334f28880c5dce4d8f44bda26a34fdfae6a6
Pyrrh <Pyrrh@users.noreply.github.com>
2017-12-26 19:55:56 -0800

    basic menu (#4)
    *created menu
    * resolved absolute import error & wrote example test (not relevant to project & only there for reference)

---
+++ src/Utilities.py
@@ -0,0 +1,9 @@
+
+class Utilities():
+
+
+    def validate(string):
+        if (string != 1 and string != 2 and string != 3 and string != 4):
+            return False
+        else:
+            return True
--- src/main.py
+++ src/main.py
@@ -1,9 +1,34 @@
-#imports
-from Recipe import Recipe
 
+
+if __name__ == '__main__' and __package__ is None:
+    from os import sys, path
+    sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+#imports
+from src.Recipe import Recipe
+from src.Utilities import Utilities
 
 if __name__ == "__main__":
     a_recipe = Recipe()
 
+    isValid = False
 
-    print("") #placeholder for later kthxbai
+
+    print("MAIN MENU\n\n")
+    print("Select an option: \n")
+
+    print("""
+    1. Add a recipe (NOT IMPLEMENTED)
+    2. Find a recipe (NOT IMPLEMENTED)
+    3. Display a recipe (NOT IMPLEMENTED, DEPENDS ON 2. ABOVE)
+    4. Delete a recipe (NOT IMPLEMENTED, DEPENDS ON 2. ABOVE)
+    5. Build a shopping list (NOT IMPLEMENTED)
+    6. Save and Quit (SAVE NOT IMPLEMENTED, QUITTING IS EASY)
+    
+    """)
+    #refactor to have menu display during a single function call
+
+    while isValid != True:
+        isValid = Utilities.validate(input())
+
+
