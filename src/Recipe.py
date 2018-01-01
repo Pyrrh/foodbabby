7289c1292f2ba23b9c07fe7edc82c7b04d70ad9d
Pyrrh <kurzen2@pdx.edu>
2018-01-01 06:55:00 -0800

    created add_ingredients method & unit test

--- src/Recipe.py
+++ src/Recipe.py
@@ -1,29 +1,30 @@
 class Recipe(object):
 
     def __init__(self):
-        #self.quantity = 0
-        #self.unit = ""
-        #self.ingredient = ""
-        self.ingredients = [] #need to store lots of qty & unit & ingred per recipe. will be ordered triple
-        self.directions = "" #how to make this food
-        self.notes = "" #personal annotations re: this recipe
-        self.recipe_name = "" #what's in a name?
-        self.synopsis = "" #prep time, cook time, bake temp, etc. also short note for easy ref if you want
-        self.uses = 0 #track how many times a recipe has been used
-        self.source = "" #where did the recipe come from?
-        self.labels = [] #used in searching for recipes
+        self.ingredients = [] # need to store lots of qty & unit & ingred per recipe. will be ordered triple
+        self.directions = "" # how to make this food
+        self.notes = "" # personal annotations re: this recipe
+        self.recipe_name = "" # what's in a name?
+        self.synopsis = "" # prep time, cook time, bake temp, etc. also short note for easy ref if you want
+        self.uses = 0 # track how many times a recipe has been used
+        self.source = "" # where did the recipe come from?
+        self.labels = [] # used in searching for recipes
 
-#these methods should interact with the database
-    #do we need these to go somewhere else? do they make sense as part of this class?
+# these methods should interact with the database
+    # do we need these to go somewhere else? do they make sense as part of this class?
 
-#####Database is for future iteration. For now, let's read & write to JSON
+#     Database is for future iteration. For now, let's read & write to JSON
     def add_recipe(self):
         again = "y"
         self.recipe_name = input("Please enter recipe name")
         while again != "n":
-            #call add_ingredient method and add this line of input here
-            input("""Please enter ingredient quantity, unit, and name. For example: 
-        '2 cups flour'""")
+
+            # get ingredient input, split it, store it
+            split_input = input("""Please enter ingredient quantity, unit, and name. For example: 
+        '2 cups flour'""").split(" ")
+            if len(split_input) == 3:
+                self.add_ingredient(split_input[0], split_input[1], split_input[2])
+
             again = input("Are there more ingredients? Y/N").lower()
         input("Please enter all recipe directions")
         input("Please enter any notes for this recipe. If there are none, just press Enter")
@@ -42,7 +43,7 @@ class Recipe(object):
         return
 
     def edit_recipe(self):
-        #maybe I just call add_recipe again and force-overwrite it????? probably a bad idea
+        # maybe I just call add_recipe again and force-overwrite it????? probably a bad idea
         return
 
     def delete_recipe(self):
@@ -51,7 +52,9 @@ class Recipe(object):
     def display_recipe(self):
         return
 
-    #we also will want a 'display all recipes' method but this belongs elsewhere
-    #same for searching for recipes
+    # we also will want a 'display all recipes' method but this belongs elsewhere
+    # same for searching for recipes
 
-
+    def add_ingredient(self, qty, unit, name):
+        self.ingredients.append((qty, unit, name))
+        return
--- tst/main_test.py
+++ tst/main_test.py
@@ -1,8 +1,24 @@
-#Import testing suite & do the things
+# Import testing suite & do the things
 import unittest
+from src.Recipe import Recipe
+from src.Utilities import Utilities
 
 
-#modify this base test case
-class my_test(unittest.TestCase):
-    def test(self):
-        self.assertEqual(3, 3)
+# modify this base test case
+
+class TestAddIngredient(unittest.TestCase):
+    def test_add_ingredient(self):
+        test_recipe = Recipe()
+
+        test_recipe.add_ingredient(1, "cup", "flour")
+
+        self.assertEqual(test_recipe.ingredients, [(1, "cup", "flour")])
+
+        test_recipe.add_ingredient(3, "Tbsp", "cinnamon")
+
+        self.assertEqual(test_recipe.ingredients, [(1, "cup", "flour"), (3, "Tbsp", "cinnamon")])
+
+
+class TestAddRecipe(unittest.Testcase):
+    def test_add_recipe(self):
+        test_recipe = Recipe()
