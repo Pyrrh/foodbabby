861778b153e0259d38f3126d73e50086e1c0dcb7
Pyrrh <kurzen2@pdx.edu>
2018-01-01 07:11:44 -0800

    catching all data in add_recipe

--- src/Recipe.py
+++ src/Recipe.py
@@ -1,14 +1,14 @@
 class Recipe(object):
 
     def __init__(self):
-        self.ingredients = [] # need to store lots of qty & unit & ingred per recipe. will be ordered triple
-        self.directions = "" # how to make this food
-        self.notes = "" # personal annotations re: this recipe
-        self.recipe_name = "" # what's in a name?
-        self.synopsis = "" # prep time, cook time, bake temp, etc. also short note for easy ref if you want
-        self.uses = 0 # track how many times a recipe has been used
-        self.source = "" # where did the recipe come from?
-        self.labels = [] # used in searching for recipes
+        self.ingredients = []  # need to store lots of qty & unit & ingred per recipe. will be ordered triple
+        self.directions = ""  # how to make this food
+        self.notes = ""  # personal annotations re: this recipe
+        self.recipe_name = ""  # what's in a name?
+        self.synopsis = ""  # prep time, cook time, bake temp, etc. also short note for easy ref if you want
+        self.uses = 0  # track how many times a recipe has been used
+        self.source = ""  # where did the recipe come from?
+        self.labels = []  # used in searching for recipes
 
 # these methods should interact with the database
     # do we need these to go somewhere else? do they make sense as part of this class?
@@ -26,24 +26,20 @@ class Recipe(object):
                 self.add_ingredient(split_input[0], split_input[1], split_input[2])
 
             again = input("Are there more ingredients? Y/N").lower()
-        input("Please enter all recipe directions")
-        input("Please enter any notes for this recipe. If there are none, just press Enter")
-        input("Please enter any recipe synopsis you would like to have. This may include"
-              " prep time, cook time, baking temperature, or more. If there is none, "
-              "just press Enter")
-        input("Please enter the recipe source. If you don't want to add one, just press Enter")
+        self.directions = input("Please enter all recipe directions")
+        self.notes = input("Please enter any notes for this recipe. If there are none, just press Enter")
+        self.synopsis = input("Please enter any recipe synopsis you would like to have. This may include"
+                              " prep time, cook time, baking temperature, or more. If there is none, "
+                              "just press Enter")
+        self.source = input("Please enter the recipe source. If you don't want to add one, just press Enter")
         more_labels = "y"
         while more_labels != "n":
-            input("Enter a label to tag this recipe with. If you don't want to add one, just press Enter")
+            self.labels.append(input("Enter a label to tag this recipe with. "
+                                     "If you don't want to add one, just press Enter"))
             more_labels = input("Is there another label to add? Y/N").lower()
         return
 
-    def get_ingredients(self):
-
-        return
-
     def edit_recipe(self):
-        # maybe I just call add_recipe again and force-overwrite it????? probably a bad idea
         return
 
     def delete_recipe(self):
