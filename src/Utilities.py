99c2adbc608ca2d988624c9527f1d7a9384f9ee0
Pyrrh <kurzen2@pdx.edu>
2018-03-26 14:35:04 -0700

    removed double-input bug in print_main_menu

--- src/Utilities.py
+++ src/Utilities.py
@@ -5,7 +5,14 @@ class Utilities:
     def validate_selection(num):
 
         if num.isalpha() or int(num) < 1 or int(num) > 6:
-            print("Invalid option. Please choose a number from the menu\n")
+            print("""Invalid option. Please choose a number (1-6) from the menu: 
+            1. Add a recipe (NOT IMPLEMENTED)
+            2. Find a recipe (NOT IMPLEMENTED)
+            3. Display a recipe (NOT IMPLEMENTED)
+            4. Delete a recipe (NOT IMPLEMENTED)
+            5. Build a shopping list (NOT IMPLEMENTED)
+            6. Save and Quit (LOAD FROM FILE NOT IMPLEMENTED)
+            """)
             return False
         else:
             return True
@@ -30,7 +37,8 @@ class Utilities:
         selection = input()
         while not is_valid:
             is_valid = Utilities.validate_selection(selection)
-            selection = input()
+            if not is_valid:
+                selection = input()
         return selection
 
     def again(self):
