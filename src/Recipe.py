6c93dc2d756f343a505964ba7be35dc0886e556f
Pyrrh <kurzen2@pdx.edu>
2018-05-29 17:39:11 -0700

    forgot to make a branch, committing to fix

--- src/Recipe.py
+++ src/Recipe.py
@@ -1,14 +1,15 @@
 class Recipe(object):
 
-    def __init__(self):
-        self.ingredients = []  # need to store lots of qty & unit & ingred per recipe. will be ordered triple
-        self.directions = ""  # how to make this food
-        self.notes = ""  # personal annotations re: this recipe
-        self.recipe_name = ""  # what's in a name?
-        self.synopsis = ""  # prep time, cook time, bake temp, etc. also short note for easy ref if you want
-        self.uses = 0  # track how many times a recipe has been used
-        self.source = ""  # where did the recipe come from?
-        self.labels = []  # used in searching for recipes
+    def __init__(self, directions="", ingredients=[], labels=[], notes="", recipe_name="",
+                 source="", synopsis="", uses=0):
+        self.directions = directions  # how to make this food
+        self.ingredients = ingredients  # need to store lots of qty & unit & ingred per recipe. will be ordered triple
+        self.labels = labels  # used in searching for recipes
+        self.notes = notes  # personal annotations re: this recipe
+        self.recipe_name = recipe_name  # what's in a name?
+        self.source = source  # where did the recipe come from?
+        self.synopsis = synopsis  # prep time, cook time, bake temp, etc. also short note for easy ref if you want
+        self.uses = uses  # track how many times a recipe has been used
 
     def add_ingredient(self, qty, unit, name):
         self.ingredients.append((qty, unit, name))
--- tst/main_test.py
+++ tst/main_test.py
@@ -8,7 +8,7 @@ from src.Cookbook import Cookbook
 # modify this base test case
 
 class TestAddIngredient(unittest.TestCase):
-    def test_add_ingredient(self):
+    def test_add_two_ingredients(self):
         test_recipe = Recipe()
 
         test_recipe.add_ingredient(1, "cup", "flour")
@@ -29,3 +29,17 @@ class TestAddRecipe(unittest.TestCase):
         # can assert more things here to ensure ingredient, directions, etc are not empty
         # this test is bad and it should feel bad
         # (tests should be automated and not take user input)
+
+class TestDisplayRecipe(unittest.TestCase):
+    def test_display_one_recipe(self):
+        test_recipe = Recipe()
+        test_recipe.directions = ""
+        test_recipe.ingredients = []
+        test_recipe.labels = []
+        test_recipe.notes = ""
+        test_recipe.recipe_name = ""
+        test_recipe.source = ""
+        test_recipe.synopsis = ""
+        test_recipe.uses = 0
+
+        self.assertEqual(test_recipe.display_recipe(), "")
